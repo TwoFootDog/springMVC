4649cb8ef381d96ef13b1906c03e2ee24b797bcc
two dog <ekfrl2815@gmail.com>
2018-11-21 01:55:13 +0900

    20181121_1

--- spring_web_project/jex01/src/main/java/org/zerock/controller/SampleController.java
+++ spring_web_project/jex01/src/main/java/org/zerock/controller/SampleController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.zerock.domain.SampleDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,7 +16,7 @@ public class SampleController {
 	public void basic() {
 		log.info("basic......................");
 	}
-
+	
 	@RequestMapping(value = "/basic", method = { RequestMethod.GET, RequestMethod.POST })
 	public void basicGet() {
 		log.info("basic get..........");
@@ -28,23 +26,5 @@ public class SampleController {
 	public void basicGet2() {
 		log.info("basic get only get...........");
 	}
-	
-	@GetMapping("/ex01")
-	public String ex01(SampleDTO dto) {
-		log.info(" " + dto);
-		return "ex01";
-	}
-	
-/*	@GetMapping("/ex02")
-	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
-		log.info("name : " + name);
-		log.info("age : " + age);
-		return "ex02";
-	}*/
-	
-	@GetMapping("/ex02")
-	public void ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
-		log.info("name : " + name);
-		log.info("age : " + age);
-	}
 }
+
