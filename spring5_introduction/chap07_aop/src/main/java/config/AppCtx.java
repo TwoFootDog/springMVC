affc052941ed087af7362e02aa254765e689ef8f
two dog <ekfrl2815@gmail.com>
2018-11-21 22:30:57 +0900

    20181121_6

---
+++ spring5_introduction/chap07_aop/src/main/java/config/AppCtx.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import aspect.CacheAspect;
+import aspect.ExeTimeAspect;
+import chap07.Calculator;
+import chap07.RecCalculator;
+
+@Configuration
+//@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAspectJAutoProxy
+public class AppCtx {
+	@Bean
+	public ExeTimeAspect exeTimeAspect() {
+		return new ExeTimeAspect();
+	}
+
+	@Bean
+	public Calculator calculator() {
+		return new RecCalculator();
+	}
+}
