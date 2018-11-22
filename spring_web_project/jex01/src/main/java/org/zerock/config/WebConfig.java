bf6209dfeb501fdeb863a48f1777221b8308cdd4
two dog <ekfrl2815@gmail.com>
2018-11-23 01:43:28 +0900

    20181123_1

--- spring_web_project/jex01/src/main/java/org/zerock/config/WebConfig.java
+++ spring_web_project/jex01/src/main/java/org/zerock/config/WebConfig.java
@@ -1,5 +1,8 @@
 package org.zerock.config;
 
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -20,6 +23,12 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 	protected String[] getServletMappings() {
 		// TODO Auto-generated method stub
 		return new String[] {"/"};
+	}
+
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		// TODO Auto-generated method stub
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
 	}
 
 }
--- spring_web_project/jex01/src/main/webapp/WEB-INF/views/error_page.jsp
+++ spring_web_project/jex01/src/main/webapp/WEB-INF/views/error_page.jsp
@@ -5,7 +5,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta charset="EUC-KR">
+<meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
