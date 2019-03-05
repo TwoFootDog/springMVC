b2cf10dc9a51e45a2c236eb642be50959744d987
ekfrl2815 <ekfrl2815@naver.com>
2019-03-05 14:28:25 +0900

    20190305_1

--- spring_web_project/nxmile_approval/src/test/java/com/tpcom_apr/service/OnmsgchkServiceTests.java
+++ spring_web_project/nxmile_approval/src/test/java/com/tpcom_apr/service/OnmsgchkServiceTests.java
@@ -1,8 +1,11 @@
 package com.tpcom_apr.service;
 
+import com.commons.domain.CustomizeHeaderVO;
 import com.commons.exception.ValidException;
 import com.tpcom_apr.domain.service.OnmsgchkInputVO;
 import com.tpcom_apr.domain.service.OnmsgchkOutputVO;
+import com.tpcom_apr.domain.service.wrapper.OnmsgchkInputWrapperVO;
+import com.tpcom_apr.domain.service.wrapper.OnmsgchkOutputWrapperVO;
 import com.tpcom_apr.domain.sql.Rul_svcavl_con_tpcom_vs2001OutputVO;
 import com.tpcom_apr.mapper.Rul_svcavl_conMapper;
 import com.tpcom_apr.service.service_interface.OnmsgchkService;
@@ -32,131 +35,151 @@ public class OnmsgchkServiceTests {
         MockitoAnnotations.initMocks(this);
     }
 
-//    @Test   // 서비스 synCall 정상 호출여부 검증
-//    public void testOnmsgchkServiceOkSynccallOK() {
-//        OnmsgchkService service = mock(OnmsgchkService.class);
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        ResponseEntity<OnmsgchkOutputVO> output = service.syncCall(headers, new OnmsgchkInputVO());
-//        verify(service).syncCall(any(), any());
-//    }
-//
-//    @Test   // 서비스 내 commonInputValidChk 및 rul_svcavl_con_tpcom_vs2001 정상 호출여부 검증
-//    public void testOnmsgchkServiceInnerMethodCallOK() {
-//        /* Mock, Spy Object 선언*/
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
-//        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
-//        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
-//        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
-//
-//        /* Stub 선언*/
-//        when(input.getTrc_no()).thenReturn("1111111111");
-//        when(input.getMcht_no()).thenReturn("11111111");
-//        when(input.getDeal_dy()).thenReturn("20190128");
-//        when(input.getCrd_no()).thenReturn("1111111111111111");
-//        when(input.getMcht_biz_no()).thenReturn("1111111111");
-//        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
-//                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
-//                        "A","ON","N","1","N"));
-//
-//        /* 서비스 호출 및 검증*/
-//        ResponseEntity<OnmsgchkOutputVO> output = service.syncCall(headers, input);
-//        verify(service).syncCall(any(), any());
-//        verify(service).commonInputDataValidChk(any(), any());
-//        verify(mapper).rul_svcavl_con_tpcom_vs2001(any());
-//    }
-//
-//
-//    @Test   // 추적번호 미 유입시 7777 에러
-//    public void testOnmsgchkServiceErrorTrcNoNotInput() {
-//        /* Mock, Spy Object 선언*/
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
-//        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
-//        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
-//        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
-//
-//        /* Stub 선언*/
-////        when(inputVO.getTrc_no()).thenReturn("1111111111");
-//        when(input.getMcht_no()).thenReturn("11111111");
-//        when(input.getDeal_dy()).thenReturn("20190128");
-//        when(input.getCrd_no()).thenReturn("1111111111111111");
-//        when(input.getMcht_biz_no()).thenReturn("1111111111");
-//        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
-//                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
-//                        "A","ON","N","1","N"));
-//
-//        /* 서비스 호출 및 검증*/
-//        try{
-//            ResponseEntity<OnmsgchkOutputVO> output = service.syncCall(headers, input);
-//        } catch (ValidException e) {
-//            if(e.getAns_cd().equals("7777")) {
-//                log.info("에러코드 :  " + e.getAns_cd() + ". 에러 검증 완료");
-//            } else {
-//                throw new ValidException(headers, e.getAns_cd(), e.getAns_msg());
-//            }
-//        }
-//    }
-//
-//    @Test   // 가맹점번호 미 유입시 7730 에러
-//    public void testOnmsgchkServiceErrorMchtNoNotInput() {
-//        /* Mock, Spy Object 선언*/
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
-//        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
-//        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
-//        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
-//
-//        /* Stub 선언*/
-//        when(input.getTrc_no()).thenReturn("1111111111");
-////        when(inputVO.getMcht_no()).thenReturn("11111111");
-//        when(input.getDeal_dy()).thenReturn("20190128");
-//        when(input.getCrd_no()).thenReturn("1111111111111111");
-//        when(input.getMcht_biz_no()).thenReturn("1111111111");
-//        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
-//                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
-//                        "A","ON","N","1","N"));
-//
-//        /* 서비스 호출 및 검증*/
-//        try{
-//            ResponseEntity<OnmsgchkOutputVO> output = service.syncCall(headers, input);
-//        } catch (ValidException e) {
-//            if (e.getAns_cd().equals("7730")) {
-//                log.info("에러코드 :  " + e.getAns_cd() + ". 에러 검증 완료");
-//            } else {
-//                throw new ValidException(headers, e.getAns_cd(), e.getAns_msg());
-//            }
-//        }
-//    }
-//
-//    @Test   // 월거래일자 미 유입시 7745 에러
-//    public void testOnmsgchkServiceErrorOrgnDealDyNotInput() {
-//        /* Mock, Spy Object 선언*/
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
-//        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
-//        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
-//        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
-//
-//        /* Stub 선언*/
-//        when(input.getTrc_no()).thenReturn("1111111111");
-//        when(input.getMcht_no()).thenReturn("11111111");
-//        when(input.getDeal_dy()).thenReturn("20190128");
-//        when(input.getCrd_no()).thenReturn("1111111111111111");
-//        when(input.getMcht_biz_no()).thenReturn("1111111111");
-//        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
-//                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
-//                        "A","ON","Y","1","N"));
-//
-//        /* 서비스 호출 및 검증*/
-//        try{
-//            ResponseEntity<OnmsgchkOutputVO> output = service.syncCall(headers, input);
-//        } catch (ValidException e) {
-//            if (e.getAns_cd().equals("7745")) {
-//                log.info("에러코드 :  " + e.getAns_cd() + ". 에러 검증 완료");
-//            } else {
-//                throw new ValidException(headers, e.getAns_cd(), e.getAns_msg());
-//            }
-//        }
-//    }
+    @Test   // 서비스 synCall 정상 호출여부 검증
+    public void testOnmsgchkServiceOkSynccallOK() {
+        OnmsgchkService service = mock(OnmsgchkService.class);
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OnmsgchkOutputWrapperVO output = service.syncCall(new OnmsgchkInputWrapperVO());
+        verify(service).syncCall(any());
+    }
+
+    @Test   // 서비스 내 commonInputValidChk 및 rul_svcavl_con_tpcom_vs2001 정상 호출여부 검증
+    public void testOnmsgchkServiceInnerMethodCallOK() {
+        /* Mock, Spy Object 선언*/
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
+        OnmsgchkInputWrapperVO inputWrapperVO = mock(OnmsgchkInputWrapperVO.class);
+        CustomizeHeaderVO header = mock(CustomizeHeaderVO.class);
+        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
+        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
+        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
+
+        /* Stub 선언*/
+        when(inputWrapperVO.getHeader()).thenReturn(header);
+        when(inputWrapperVO.getBody()).thenReturn(input);
+        when(header.getTelgrm_no()).thenReturn("K410");
+        when(header.getOrgan_cd()).thenReturn("5004");
+        when(header.getTrc_no()).thenReturn("1111111111");
+        when(header.getTelgrm_fg()).thenReturn("ON");
+        when(input.getTrc_no()).thenReturn("1111111111");
+        when(input.getMcht_no()).thenReturn("11111111");
+        when(input.getDeal_dy()).thenReturn("20190128");
+        when(input.getCrd_no()).thenReturn("1111111111111111");
+        when(input.getMcht_biz_no()).thenReturn("1111111111");
+        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
+                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
+                        "A","ON","N","1","N"));
+
+        /* 서비스 호출 및 검증*/
+        OnmsgchkOutputWrapperVO output = service.syncCall(inputWrapperVO);
+        verify(service).syncCall(any());
+        verify(service).commonInputDataValidChk(any());
+        verify(mapper).rul_svcavl_con_tpcom_vs2001(any());
+    }
+
+
+    @Test   // 추적번호 미 유입시 7777 에러
+    public void testOnmsgchkServiceErrorTrcNoNotInput() {
+        /* Mock, Spy Object 선언*/
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
+        OnmsgchkInputWrapperVO inputWrapperVO = mock(OnmsgchkInputWrapperVO.class);
+        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
+        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
+        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
+
+        /* Stub 선언*/
+        when(inputWrapperVO.getBody()).thenReturn(input);
+//        when(inputVO.getTrc_no()).thenReturn("1111111111");
+        when(input.getMcht_no()).thenReturn("11111111");
+        when(input.getDeal_dy()).thenReturn("20190128");
+        when(input.getCrd_no()).thenReturn("1111111111111111");
+        when(input.getMcht_biz_no()).thenReturn("1111111111");
+        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
+                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
+                        "A","ON","N","1","N"));
+
+        /* 서비스 호출 및 검증*/
+        try{
+            OnmsgchkOutputWrapperVO output = service.syncCall(inputWrapperVO);
+        } catch (ValidException e) {
+            if(e.getAns_cd().equals("7777")) {
+                log.info("에러코드 :  " + e.getAns_cd() + ". 에러 검증 완료");
+            } else {
+                throw new ValidException(e.getAns_cd(), e.getAns_msg());
+            }
+        }
+    }
+
+    @Test   // 가맹점번호 미 유입시 7730 에러
+    public void testOnmsgchkServiceErrorMchtNoNotInput() {
+        /* Mock, Spy Object 선언*/
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
+        OnmsgchkInputWrapperVO inputWrapperVO = mock(OnmsgchkInputWrapperVO.class);
+        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
+        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
+        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
+
+        /* Stub 선언*/
+        when(inputWrapperVO.getBody()).thenReturn(input);
+        when(input.getTrc_no()).thenReturn("1111111111");
+//        when(inputVO.getMcht_no()).thenReturn("11111111");
+        when(input.getDeal_dy()).thenReturn("20190128");
+        when(input.getCrd_no()).thenReturn("1111111111111111");
+        when(input.getMcht_biz_no()).thenReturn("1111111111");
+        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
+                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
+                        "A","ON","N","1","N"));
+
+        /* 서비스 호출 및 검증*/
+        try{
+            OnmsgchkOutputWrapperVO output = service.syncCall(inputWrapperVO);
+        } catch (ValidException e) {
+            if (e.getAns_cd().equals("7730")) {
+                log.info("에러코드 :  " + e.getAns_cd() + ". 에러 검증 완료");
+            } else {
+                throw new ValidException(e.getAns_cd(), e.getAns_msg());
+            }
+        }
+    }
+
+    @Test   // 월거래일자 미 유입시 7745 에러
+    public void testOnmsgchkServiceErrorOrgnDealDyNotInput() {
+        /* Mock, Spy Object 선언*/
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OnmsgchkService service = spy(OnmsgchkServiceImpl.class);
+        OnmsgchkInputWrapperVO inputWrapperVO = mock(OnmsgchkInputWrapperVO.class);
+        CustomizeHeaderVO header = mock(CustomizeHeaderVO.class);
+        OnmsgchkInputVO input = mock(OnmsgchkInputVO.class);
+        Rul_svcavl_conMapper mapper = mock(Rul_svcavl_conMapper.class);
+        ((OnmsgchkServiceImpl) service).setRul_svcavl_conMapper(mapper);
+
+        /* Stub 선언*/
+        when(inputWrapperVO.getHeader()).thenReturn(header);
+        when(inputWrapperVO.getBody()).thenReturn(input);
+        when(header.getTelgrm_no()).thenReturn("K410");
+        when(header.getOrgan_cd()).thenReturn("5004");
+        when(header.getTrc_no()).thenReturn("1111111111");
+        when(header.getTelgrm_fg()).thenReturn("ON");
+        when(input.getTrc_no()).thenReturn("1111111111");
+        when(input.getMcht_no()).thenReturn("11111111");
+        when(input.getDeal_dy()).thenReturn("20190128");
+        when(input.getCrd_no()).thenReturn("1111111111111111");
+        when(input.getMcht_biz_no()).thenReturn("1111111111");
+        when(mapper.rul_svcavl_con_tpcom_vs2001(any())).
+                thenReturn(new Rul_svcavl_con_tpcom_vs2001OutputVO(
+                        "A","ON","Y","1","N"));
+
+        /* 서비스 호출 및 검증*/
+        try{
+            OnmsgchkOutputWrapperVO output = service.syncCall(inputWrapperVO);
+        } catch (ValidException e) {
+            if (e.getAns_cd().equals("7745")) {
+                log.info("에러코드 :  " + e.getAns_cd() + ". 에러 검증 완료");
+            } else {
+                throw new ValidException(e.getAns_cd(), e.getAns_msg());
+            }
+        }
+    }
 }
--- spring_web_project/nxmile_approval/src/test/java/com/tpcom_apr/service/OritrqryServiceTests.java
+++ spring_web_project/nxmile_approval/src/test/java/com/tpcom_apr/service/OritrqryServiceTests.java
@@ -2,6 +2,8 @@ package com.tpcom_apr.service;
 
 
 import com.commons.exception.ValidException;
+import com.tpcom_apr.domain.service.wrapper.OritrqryInputWrapperVO;
+import com.tpcom_apr.domain.service.wrapper.OritrqryOutputWrapperVO;
 import com.tpcom_apr.domain.sql.Apr_dealtr_trn_tpcom_vs2002OutputVO;
 import com.tpcom_apr.domain.service.OritrqryInputVO;
 import com.tpcom_apr.domain.service.OritrqryOutputVO;
@@ -30,77 +32,77 @@ public class OritrqryServiceTests {
 
     }
 
-//    @Test
-//    @Ignore
-//    public void testOritrqryServiceSyncCallOk() {
-//        /* Mock Object 선언 */
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        OritrqryService service = mock(OritrqryServiceImpl.class);
-//
-//        /* 검증 */
-//        ResponseEntity<OritrqryOutputVO> output = service.syncCall(headers, new OritrqryInputVO());
-//        verify(service).syncCall(any(), any());
-//
-//    }
-//
-//    @Test
-//    @Ignore
-//    public void testOritrqryServiceInnerMethodCallOk() {
-//        /* Mock Object 선언 */
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        OritrqryService service = spy(OritrqryServiceImpl.class);
-//        OritrqryInputVO input = mock(OritrqryInputVO.class);
-//        Apr_dealtr_trnMapper mapper = mock(Apr_dealtr_trnMapper.class);
-//        ((OritrqryServiceImpl) service).setApr_dealtr_trnMapper(mapper);
-//
-//        /* Stub 선언 */
-//        when(input.getSvc_modu_id()).thenReturn("ZPTUTXPTC0001");
-//        when(input.getAns_cd()).thenReturn("");
-//        when(mapper.apr_dealtr_trn_tpcom_vs2002(any()))
-//                .thenReturn(new Apr_dealtr_trn_tpcom_vs2002OutputVO(
-//                        "A","20190128","","","","","","","",""
-//                        ,"","","","",0D,0D, 0L,0L,0L, 0L
-//                        ,0L,"",0L,0L,0L,0L,0L,0L,0L,""
-//                        ,"","","","","","","","","",""
-//                        ,"","","","",0L,0L,"","","",0L
-//                        ,"","","","","","","","",0D,0L
-//                        ,"","","","","","","","","",0L));
-//        /* 검증 */
-//        ResponseEntity<OritrqryOutputVO> output = service.syncCall(headers, input);
-//        verify(service).changeOrgnAprvNo(any(), any());
-//        verify(service).sqlTypeSetting(any(), any());
-//        verify(mapper).apr_dealtr_trn_tpcom_vs2002(any());
-//    }
-//
-//    @Test
-//    @Ignore
-//    public void testOritrqryServiceDataNotFoundError() {
-//        /* Mock Object 선언 */
-//        HttpHeaders headers = mock(HttpHeaders.class);
-//        OritrqryService service = spy(OritrqryServiceImpl.class);
-//        OritrqryInputVO input = mock(OritrqryInputVO.class);
-//        Apr_dealtr_trnMapper mapper = mock(Apr_dealtr_trnMapper.class);
-//        ((OritrqryServiceImpl) service).setApr_dealtr_trnMapper(mapper);
-//
-//        /* Stub 선언 */
-//        when(input.getSvc_modu_id()).thenReturn("ZPTUTXPTC0001");
-//        when(input.getAns_cd()).thenReturn("");
-//        when(mapper.apr_dealtr_trn_tpcom_vs2002(any()))
-//                .thenReturn(null);
-//        /* 검증 */
-//        try {
-//            ResponseEntity<OritrqryOutputVO> output = service.syncCall(headers, input);
-//        } catch (ValidException e) {
-//            if (e.getAns_cd().equals("7777")) {
-//               log.info("에러코드 : " + e.getAns_cd() + ". 에러 검증 완료");
-//            } else {
-//                throw new ValidException(headers, e.getAns_cd(), e.getAns_msg());
-//            }
-//        }
-//
-//        verify(service).changeOrgnAprvNo(any(), any());
-//        verify(service).sqlTypeSetting(any(), any());
-//        verify(mapper).apr_dealtr_trn_tpcom_vs2002(any());
-//    }
+    @Test
+    @Ignore
+    public void testOritrqryServiceSyncCallOk() {
+        /* Mock Object 선언 */
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OritrqryService service = mock(OritrqryServiceImpl.class);
+
+        /* 검증 */
+        OritrqryOutputWrapperVO output = service.syncCall(new OritrqryInputWrapperVO());
+        verify(service).syncCall(any());
+
+    }
+
+    @Test
+    @Ignore
+    public void testOritrqryServiceInnerMethodCallOk() {
+        /* Mock Object 선언 */
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OritrqryService service = spy(OritrqryServiceImpl.class);
+        OritrqryInputVO input = mock(OritrqryInputVO.class);
+        Apr_dealtr_trnMapper mapper = mock(Apr_dealtr_trnMapper.class);
+        ((OritrqryServiceImpl) service).setApr_dealtr_trnMapper(mapper);
+
+        /* Stub 선언 */
+        when(input.getSvc_modu_id()).thenReturn("ZPTUTXPTC0001");
+        when(input.getAns_cd()).thenReturn("");
+        when(mapper.apr_dealtr_trn_tpcom_vs2002(any()))
+                .thenReturn(new Apr_dealtr_trn_tpcom_vs2002OutputVO(
+                        "A","20190128","","","","","","","",""
+                        ,"","","","",0D,0D, 0L,0L,0L, 0L
+                        ,0L,"",0L,0L,0L,0L,0L,0L,0L,""
+                        ,"","","","","","","","","",""
+                        ,"","","","",0L,0L,"","","",0L
+                        ,"","","","","","","","",0D,0L
+                        ,"","","","","","","","","",0L));
+        /* 검증 */
+        ResponseEntity<OritrqryOutputVO> output = service.syncCall(headers, input);
+        verify(service).changeOrgnAprvNo(any(), any());
+        verify(service).sqlTypeSetting(any(), any());
+        verify(mapper).apr_dealtr_trn_tpcom_vs2002(any());
+    }
+
+    @Test
+    @Ignore
+    public void testOritrqryServiceDataNotFoundError() {
+        /* Mock Object 선언 */
+        HttpHeaders headers = mock(HttpHeaders.class);
+        OritrqryService service = spy(OritrqryServiceImpl.class);
+        OritrqryInputVO input = mock(OritrqryInputVO.class);
+        Apr_dealtr_trnMapper mapper = mock(Apr_dealtr_trnMapper.class);
+        ((OritrqryServiceImpl) service).setApr_dealtr_trnMapper(mapper);
+
+        /* Stub 선언 */
+        when(input.getSvc_modu_id()).thenReturn("ZPTUTXPTC0001");
+        when(input.getAns_cd()).thenReturn("");
+        when(mapper.apr_dealtr_trn_tpcom_vs2002(any()))
+                .thenReturn(null);
+        /* 검증 */
+        try {
+            ResponseEntity<OritrqryOutputVO> output = service.syncCall(headers, input);
+        } catch (ValidException e) {
+            if (e.getAns_cd().equals("7777")) {
+               log.info("에러코드 : " + e.getAns_cd() + ". 에러 검증 완료");
+            } else {
+                throw new ValidException(headers, e.getAns_cd(), e.getAns_msg());
+            }
+        }
+
+        verify(service).changeOrgnAprvNo(any(), any());
+        verify(service).sqlTypeSetting(any(), any());
+        verify(mapper).apr_dealtr_trn_tpcom_vs2002(any());
+    }
 
 }
